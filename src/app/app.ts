d0d0747985285ffc813c765826cad6042e0a2923
Jason Jean <jjean@forbes.com>
2016-09-18 15:13:51 -0400

    Scripts aren't working

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app',
   template: `
   <p>Hello Angular Universal App</p>
+  <script>console.log('I don't work')</script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <router-outlet></router-outlet>
   `
 })
