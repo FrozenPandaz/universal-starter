ff7cb6c31c204e0e3abf1de61786f5ee2c5f3ae2
Jason Jean <jjean@forbes.com>
2016-09-19 18:13:10 -0400

    Add sanitizer example

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DomSanitizationService } from '@angular/core';
 
 @Component({
   selector: 'app',
@@ -6,11 +6,14 @@ import { Component } from '@angular/core';
   <p>Hello Angular Universal App</p>
   <script>console.log('I don't work')</script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script [attr.src]="sanitizer.bypassSecurityTrustResourceUrl(script_url)"
   <router-outlet></router-outlet>
   `
 })
 export class App {
+  constructor(private sanitizer: DomSanitizationService) {}
 
+  script_url: string = 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js';
 }
 
 @Component({
